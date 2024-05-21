9f7b9c0be45f97285da039ec462f4a4a4f5a0a4d
NataliaAst <al241924@alumnos.uacj.mx>
2024-05-21 01:53:33 -0600

    Opción multiple

--- Escape-room-HCJ/script.js
+++ Escape-room-HCJ/script.js
@@ -1,6 +1,10 @@
-function checkAnswer1() {
-    const answer1 = document.getElementById('answer1').value.toLowerCase();
-    if (answer1 === 'ajo' || answer1 === 'una almohada') {
+function startGame() {
+    document.getElementById('start-screen').style.display = 'none';
+    document.getElementById('room1').style.display = 'block';
+}
+
+function checkAnswer1(answer) {
+    if (answer === 'ajo') {
         document.getElementById('room1').style.display = 'none';
         document.getElementById('room2').style.display = 'block';
     } else {
@@ -8,9 +12,8 @@ function checkAnswer1() {
     }
 }
 
-function checkAnswer2() {
-    const answer2 = document.getElementById('answer2').value.toLowerCase();
-    if (answer2 === 'una vela') {
+function checkAnswer2(answer) {
+    if (answer === 'una vela') {
         document.getElementById('room2').style.display = 'none';
         document.getElementById('room3').style.display = 'block';
     } else {
